00957ed34e3bbc9596907d009c2fdea4e3ec5b64
hud10837 <hud10837@esri.com>
2020-06-25 14:39:43 +0100

    Add cmake files

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/src/chatbot.cpp" "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/src/chatgui.cpp" "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/src/chatlogic.cpp" "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/src/graphedge.cpp" "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/src/graphnode.cpp" "/Users/hud10837/cppCourse/chatbot/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXMAC__"
+  "__WXOSX_COCOA__"
+  "__WXOSX__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/osx_cocoa-unicode-3.0"
+  "/usr/local/include/wx-3.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
