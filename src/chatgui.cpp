5b9d9110dd53a21a3173d47f26f8485be64b7ca7
a <you@example.com>
2020-10-15 18:19:33 +0200

    Hopefully completed task 1

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,8 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    //_chatLogic = new ChatLogic();
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -134,8 +135,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
 {
     //// STUDENT CODE
     ////
-
-    delete _chatLogic;
+    //deleted this line
+    //delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
--- src/chatgui.h
+++ src/chatgui.h
@@ -2,7 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
-
+#include <memory>
 class ChatLogic; // forward declaration
 
 // middle part of the window containing the dialog between user and chatbot
@@ -16,7 +16,9 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    //ChatLogic *_chatLogic;
+    //My code
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +29,13 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    //Chatlogic *GetChatLogicHandle() { return _chatLogic; }
+    std::unique_ptr<ChatLogic>& GetChatLogicHandle()
+    {
+        return(_chatLogic);
+    };
+
+    void foo(){};
 
     // events
     void paintEvent(wxPaintEvent &evt);
