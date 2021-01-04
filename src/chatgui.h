99babdcf4174a5ed83f7090932ea3803b015eed9
Wanting Zhang <wantingzhang@wantings-mbp.lan>
2021-01-04 15:37:36 -0800

    finish task 1

--- src/chatgui.h
+++ src/chatgui.h
@@ -16,7 +16,7 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +27,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    std::unique_ptr<ChatLogic> GetChatLogicHandle() { return std::move(_chatLogic); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
