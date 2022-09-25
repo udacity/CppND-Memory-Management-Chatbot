c473434ec55a32ba426c3ce9eb2019d44eb6a30d
strikingDimas <dasiedimasakbar@gmail.com>
2022-09-25 14:33:22 +0900

    task 1 : exclusive ownership 1

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,8 +118,8 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
-
+    _chatLogic = std::make_unique<ChatLogic>(); 
+    
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
 
@@ -135,7 +135,7 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    std::cout << "ChatBotPanelDialog Destructor\n";
 
     ////
     //// EOF STUDENT CODE
