a0c92cf15937b2aec9f569087aa52b8275fb5804
Yu Xiong <xiongyu1988@gmail.com>
2024-02-18 22:42:21 -0800

    add std::unique_ptr<ChatLogic> _chatLogic

--- src/chatgui.h
+++ src/chatgui.h
@@ -16,7 +16,8 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    //ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
